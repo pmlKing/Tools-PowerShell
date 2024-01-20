8247d6e31b2660ffe12e875c6d58b38b7d62bc24
pmlKing <157214154+pmlKing@users.noreply.github.com>
2024-01-20 17:18:43 +0530

    Update ListADOTeamMembers.ps1

--- ListADOTeamMembers.ps1
+++ ListADOTeamMembers.ps1
@@ -2,16 +2,21 @@
 
 $UriOrganization = "https://dev.azure.com/OrgName/"
 
+#Get all Projects
 $ProjectsResult = az devops project list  
 
 $Projects  = $ProjectsResult| ConvertFrom-Json
 
 'ProjectId'+  "`t"+ 'ProjectName'+ "`t"+'Team.id'+"`t"+ 'Team.name' + "`t"+ 'TeamMember.id'+ "`t"+'TeamMember.displayName'+"`t"+ 'TeamMember.uniqueName'
 
+#Iterate through all Projects
 Foreach ($projectId in $Projects.value.id )
 {
      
+        #Get Teams List 
         $TeamsResult = ( (az devops team list  --org $UriOrganization --project $projectId ) | ConvertFrom-Json ) 
+        
+        #Iterate through each Team 
          Foreach ($Team in  $TeamsResult )
          {
             $TeamMembersResult = (az devops team list-member --team $Team.id --project  $projectId ) | ConvertFrom-Json 
